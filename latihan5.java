1809717782593235ed9e96be5ea99418f1669696
Irsyal Rizky <121174791+Irsyal02@users.noreply.github.com>
2026-04-30 11:32:45 +0700

    Refactor ManajemenNilaiMahasiswa to PB class

--- latihan5.java
+++ latihan5.java
@@ -1,7 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pb;
+
 import java.util.Scanner;
 
-public class ManajemenNilaiMahasiswa {
-    public static void main(String[] args) {
+/**
+ *
+ * @author MAHASISWA
+ */
+public class PB {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // 1. Input jumlah kelas, mahasiswa, dan semester
@@ -15,20 +29,23 @@ public class ManajemenNilaiMahasiswa {
         // Inisialisasi Array 3 Dimensi: nilai[kelas][mahasiswa][semester]
         double[][][] nilai = new double[jmlKelas][jmlMahasiswa][jmlSemester];
 
-        System.out.println("\n--- Input Data Nilai ---");
+        System.out.println("\n=== INPUT DATA NILAI ===");
         // 2. Mengisi nilai mahasiswa
         for (int i = 0; i < jmlKelas; i++) {
+                    System.out.printf("Nilai Kelas %d ", (i + 1));
+                    System.out.println();
             for (int j = 0; j < jmlMahasiswa; j++) {
                 for (int k = 0; k < jmlSemester; k++) {
-                    System.out.printf("Nilai Kelas %d, Mahasiswa %d, Semester %d: ", (i + 1), (j + 1), (k + 1));
-                    nilai[i][j][k] = input.nextDouble();
+                    System.out.printf(" Mahasiswa %d, Semester %d: ", (j + 1), (k + 1));
+                    
+                    nilai[i][j][k] = input.nextDouble() ;
                 }
             }
             System.out.println(); // Spasi antar kelas
         }
 
         // 3. Menampilkan seluruh data nilai
-        System.out.println("--- Seluruh Data Nilai ---");
+        System.out.println("\n=== SELURUH DATA NILAI ===");
         for (int i = 0; i < jmlKelas; i++) {
             System.out.println("Kelas " + (i + 1) + ":");
             for (int j = 0; j < jmlMahasiswa; j++) {
@@ -41,7 +58,7 @@ public class ManajemenNilaiMahasiswa {
         }
 
         // 4. Menghitung rata-rata nilai per kelas
-        System.out.println("\n--- Rata-rata Nilai Per Kelas ---");
+        System.out.println("\n== RATA-RATA NILAI PER KELAS");
         for (int i = 0; i < jmlKelas; i++) {
             double totalPerKelas = 0;
             int jumlahDataPerKelas = jmlMahasiswa * jmlSemester;
@@ -57,8 +74,8 @@ public class ManajemenNilaiMahasiswa {
         }
 
         input.close();
-    }
 }
-
+         
+}
 
 
